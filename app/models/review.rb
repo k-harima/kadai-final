755e80de529d9076e2b549fe767a53c27348923f
yosuke.yomosue <stbrimaha@gmail.com>
2021-09-17 11:24:50 +0000

    fix レイアウト

--- app/models/review.rb
+++ app/models/review.rb
@@ -6,6 +6,6 @@ class Review < ApplicationRecord
                                       less_than_or_equal_to: 5,
                                       greater_than_or_equal_to: 1,
                                     }
-  validates :title, presence: true, length: { maximum: 20 }
+  validates :title, presence: true, length: { maximum: 50 }
   validates :text, presence: true, length: { maximum: 500 }
 end
